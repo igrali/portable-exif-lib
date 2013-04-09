664eaa7df46931faced0aa72a23ee9bad6f95c75
igrali <igrali@outlook.com>
2013-04-09 17:53:22 +0200

    WP8 test app

---
+++ WP8TestApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WP8TestApp.Resources;
+
+namespace WP8TestApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ WP8TestApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using ExifLib;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+using WP8TestApp.Resources;
+
+namespace WP8TestApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var task = new PhotoChooserTask();
+            task.Completed += task_Completed;
+            task.Show();
+        }
+
+        void task_Completed(object sender, PhotoResult e)
+        {
+            var jpeginfo = ExifReader.ReadJpeg(e.ChosenPhoto);
+            this.DataContext = jpeginfo;
+        }
+    }
+}
